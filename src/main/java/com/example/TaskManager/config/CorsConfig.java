946678477f5f9170fa362ee3fa035d20a78b9fef
Niranjan755 <59230111+Niranjan755@users.noreply.github.com>
2025-06-24 22:12:44 -0500

    Create CorsConfig.java

---
+++ src/main/java/com/example/TaskManager/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.TaskManager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://task-manager-frontend-git-main-niranjan755s-projects.vercel.app") // <== your React app deployed URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
